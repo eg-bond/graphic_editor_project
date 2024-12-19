d91f00431f182ccacf46650ad2be3e915362a05e
eg-bond <eg-bond@yandex.ru>
2024-12-19 18:52:01 +0300

    feat: all menu elements (prototypes) added

--- src/components/HistoryMenu/HistoryMenu.tsx
+++ src/components/HistoryMenu/HistoryMenu.tsx
@@ -1,7 +1,17 @@
+import { Divider } from 'antd';
+
 export function HistoryMenu() {
   return (
     <div className='h-1/2'>
       <h1>History Menu</h1>
+      <Divider className='m-0' />
+      <div className='history'>
+        <div className='flex flex-col justify-between'>
+          <div>Действие 1</div>
+          <div>Действие 2</div>
+          <div>Действие 3</div>
+        </div>
+      </div>
     </div>
   );
 }
