11e85bad4e72ce6d81bf133d6867c6693ab3ac7b
eg-bond <eg-bond@yandex.ru>
2024-12-19 17:43:20 +0300

    feat: temp route added

--- src/components/Routing/Routing.tsx
+++ src/components/Routing/Routing.tsx
@@ -1,4 +1,5 @@
 import { EditorPage } from '@/pages/EditorPage';
+import { EditorPageTemp } from '@/pages/EditorPageTemp';
 import { AppRoutes } from '@/types/appRoutes';
 import { Route, Routes } from 'react-router-dom';
 import { MainPage } from '@/pages/MainPage';
@@ -12,6 +13,11 @@ export function Routing() {
         element={<h1 className='text-4xl'>Projects Page</h1>}
       />
       <Route path={AppRoutes.CurrentProject} element={<EditorPage />} />
+      <Route
+        path={AppRoutes.CurrentProject}
+        element={<h1 className='text-4xl'>Current project Page</h1>}
+      />
+      <Route path={AppRoutes.CurrentProjectTemp} element={<EditorPageTemp />} />
     </Routes>
   );
 }
