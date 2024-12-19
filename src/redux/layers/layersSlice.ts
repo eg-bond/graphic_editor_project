779bb0362a9a505d524f23a8f5544d2b1f29094a
eg-bond <eg-bond@yandex.ru>
2024-12-19 17:34:25 +0300

    feat: slice for storing layers data created

---
+++ src/redux/layers/layersSlice.ts
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState: string[] = []
+
+export const layersSlice = createSlice({
+  name: 'layers',
+  initialState,
+  reducers: {
+  },
+});
