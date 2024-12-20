52950a9a34d3171ee315360128da26970f4d9adb
eg-bond <eg-bond@yandex.ru>
2024-12-20 16:44:11 +0300

    feat: logic for changing layer name done

---
+++ src/components/LayersMenu/LayerName.tsx
@@ -0,0 +1,58 @@
+import { useAppDispatch } from '@/redux/hooks';
+import { activateLayer, changeLayerName } from '@/redux/layers';
+import { LayerT } from '@/redux/layers/layersSlice';
+import { Button } from 'antd';
+import { ChangeEvent, FormEvent, useRef } from 'react';
+
+type ILayerName = {
+  id: LayerT['id'];
+  name: LayerT['name'];
+  isInputVisible: boolean;
+  setIsInputVisible: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function LayerName({
+  id,
+  name,
+  isInputVisible,
+  setIsInputVisible,
+}: ILayerName) {
+  const d = useAppDispatch();
+
+  const input = useRef('');
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> & FormEvent<HTMLFormElement>
+  ) => {
+    input.current = e.target.value;
+  };
+
+  const handleSubmit = (
+    e: ChangeEvent<HTMLInputElement> & FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+    d(changeLayerName({ id, name: input.current }));
+    setIsInputVisible(false);
+  };
+
+  return (
+    <>
+      {isInputVisible && (
+        <form onSubmit={handleSubmit} onChange={handleChange}>
+          <input
+            name='change_layer_name'
+            type='text'
+            className='flex-grow'
+            onBlur={handleSubmit}
+            autoFocus
+          />
+        </form>
+      )}
+      {!isInputVisible && (
+        <Button className='flex-grow' onClick={() => d(activateLayer(id))}>
+          {name}
+        </Button>
+      )}
+    </>
+  );
+}
