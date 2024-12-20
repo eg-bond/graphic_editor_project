34c767a9a5b3724beff2623cf2c0e4177bc5126c
eg-bond <eg-bond@yandex.ru>
2024-12-20 11:45:06 +0300

    feat: moved layer item to its own component

--- src/components/LayersMenu/LayersList.tsx
+++ src/components/LayersMenu/LayersList.tsx
@@ -1,44 +1,16 @@
-import { useAppDispatch } from '@/redux/hooks';
-import { removeLayer } from '@/redux/layers';
-import { Layer } from '@/redux/layers/layersSlice';
-import { Button, Dropdown } from 'antd';
+import { LayerT } from '@/redux/layers/layersSlice';
+import { Layer } from './Layer';
 
-interface ILayersProps {
-  layers: Array<Layer>;
+interface ILayersListProps {
+  layers: Array<LayerT>;
 }
 
-export function LayersList({ layers }: ILayersProps) {
-  const d = useAppDispatch();
-
-  const dropdownContent = (layerId: number) => {
-    return (
-      <div className='bg-white rounded shadow-lg p-2 min-w-[200px]'>
-        <button className='w-full text-left px-3 py-2 hover:bg-gray-100 rounded'>
-          Переименовать
-        </button>
-        <button
-          className='w-full text-left px-3 py-2 hover:bg-gray-100 rounded'
-          onClick={() => d(removeLayer(layerId))}>
-          Удалить
-        </button>
-      </div>
-    );
-  };
-
+export function LayersList({ layers }: ILayersListProps) {
   return (
     <div className='layers'>
       <div className='flex flex-col justify-between'>
         {layers.map(layer => (
-          <div key={layer.id}>
-            <span>{layer.name}</span>
-            <button>Скрыть</button>
-            <Dropdown
-              dropdownRender={() => dropdownContent(layer.id)}
-              placement='bottomLeft'
-              trigger={['click']}>
-              <Button>...</Button>
-            </Dropdown>
-          </div>
+          <Layer id={layer.id} name={layer.name} />
         ))}
       </div>
     </div>
