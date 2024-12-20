585cd5aef7d2a1a752ba6e87e74d53a46337c45f
eg-bond <eg-bond@yandex.ru>
2024-12-20 16:52:10 +0300

    feat: slice for storing history created and connected to store and HistoryMenu component

---
+++ src/redux/history/index.ts
@@ -0,0 +1,5 @@
+import { historySlice } from './historySlice';
+
+export const historyReducer = historySlice.reducer;
+
+// export const {} = historySlice.actions;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { protoReducer } from './proto';
 import { layersReducer } from './layers';
+import { historyReducer } from './history';
 
 export const store = configureStore({
   // TODO: remove protoReducer later
-  reducer: { proto: protoReducer, layers: layersReducer },
+  reducer: {
+    proto: protoReducer,
+    layers: layersReducer,
+    history: historyReducer,
+  },
 
   devTools: true,
 });
