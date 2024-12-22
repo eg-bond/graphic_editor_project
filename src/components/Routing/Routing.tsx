78af2eebd017dfe0242b9405dfafc3bfe65e53b9
FarhodD <farhoddavlatov0120@gmail.com>
2024-12-22 14:03:04 +0500

    feat: ddone navigation

--- src/components/Routing/Routing.tsx
+++ src/components/Routing/Routing.tsx
@@ -1,19 +1,23 @@
 import { EditorPage } from '@/pages/EditorPage';
 import { AppRoutes } from '@/types/appRoutes';
 import { Route, Routes } from 'react-router-dom';
+import Navigation from './Navigation';
 
 export function Routing() {
   return (
-    <Routes>
-      <Route
-        path={AppRoutes.Main}
-        element={<h1 className='text-4xl'>Main Page</h1>}
-      />
-      <Route
-        path={AppRoutes.Projects}
-        element={<h1 className='text-4xl'>Projects Page</h1>}
-      />
-      <Route path={AppRoutes.CurrentProject} element={<EditorPage />} />
-    </Routes>
+    <>
+      <Navigation />
+      <Routes>
+        <Route
+          path={AppRoutes.Main}
+          element={<h1 className='text-4xl'>Main Page</h1>}
+        />
+        <Route
+          path={AppRoutes.Projects}
+          element={<h1 className='text-4xl'>Projects Page</h1>}
+        />
+        <Route path={AppRoutes.CurrentProject} element={<EditorPage />} />
+      </Routes>
+    </>
   );
 }
