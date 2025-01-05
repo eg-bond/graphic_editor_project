480bd15c5a5ca4675189ac0879a78122de25000b
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2025-01-05 11:54:55 +0500

    feat: added canvas rendering with specified resolution

--- src/components/Navigation/CreateProjectButton.tsx
+++ src/components/Navigation/CreateProjectButton.tsx
@@ -1,39 +1,50 @@
-import { FC, memo, useCallback } from 'react';
-import { useModal } from '@/hooks/useModal.tsx';
-import { CreateProjectModal, Project } from '@/components/CreateProjectModal';
-import { Form } from 'antd';
-import { PROJECTS_KEY } from '@/utils/constants.ts';
-import { getUid } from '@/utils/getUid.ts';
-import { useNavigate } from 'react-router-dom';
+import { FC, memo, useCallback } from "react";
+import { useModal } from "@/hooks/useModal.tsx";
+import { CreateProjectModal, Project } from "@/components/CreateProjectModal";
+import { Form } from "antd";
+import { PROJECTS_KEY } from "@/utils/constants.ts";
+import { getUid } from "@/utils/getUid.ts";
+import { useNavigate } from "react-router-dom";
 
 const CreateProjectButton1: FC = () => {
   const navigate = useNavigate();
   const [form] = Form.useForm();
   const { open, onOpen, onClose } = useModal();
 
-  const handleSubmit = useCallback((values: Omit<Project, 'id'>) => {
-    const projects = JSON.parse(localStorage.getItem(PROJECTS_KEY) ?? '[]');
+  const handleSubmit = useCallback(
+    (values: Omit<Project, "id">) => {
+      const projects = JSON.parse(localStorage.getItem(PROJECTS_KEY) ?? "[]");
 
-    const id = getUid();
-    const newProjects = [{ id, height: +values.height, width: +values.width, name: values.name }, ...projects];
+      const id = getUid();
+      const newProject = {
+        id,
+        height: +values.height,
+        width: +values.width,
+        name: values.name
+      };
+      const newProjects = [newProject, ...projects];
 
-    localStorage.setItem(PROJECTS_KEY, JSON.stringify(newProjects));
+      localStorage.setItem(PROJECTS_KEY, JSON.stringify(newProjects));
 
-    form.resetFields();
-    onClose();
-    navigate(`/projects/${id}`);
-  }, [form, onClose, navigate]);
+      form.resetFields();
+      onClose();
+      navigate(`/projects/${id}`, { state: newProject });
+    },
+    [form, onClose, navigate]
+  );
 
   return (
     <>
-      <button onClick={onOpen}>
-        Новый проект
-      </button>
+      <button onClick={onOpen}>Новый проект</button>
 
-      <CreateProjectModal open={open} onClose={onClose} form={form} handleSubmit={handleSubmit} />
+      <CreateProjectModal
+        open={open}
+        onClose={onClose}
+        form={form}
+        handleSubmit={handleSubmit}
+      />
     </>
-
   );
-}
+};
 
 export const CreateProjectButton = memo(CreateProjectButton1);
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,18 +1,18 @@
-import { FC, memo, MouseEventHandler, useCallback, useState } from 'react';
-import { Button, Card, Col, Form, Popconfirm, Row, Typography } from 'antd';
-import { Gutter } from 'antd/es/grid/row';
-import { getUid } from '@/utils/getUid.ts';
-import { Link, useNavigate } from 'react-router-dom';
-import { CreateProjectModal, Project } from '@/components/CreateProjectModal';
-import { useModal } from '@/hooks/useModal.tsx';
-import { PROJECTS_KEY } from '@/utils/constants.ts';
+import { FC, memo, MouseEventHandler, useCallback, useState } from "react";
+import { Button, Card, Col, Form, Popconfirm, Row, Typography } from "antd";
+import { Gutter } from "antd/es/grid/row";
+import { getUid } from "@/utils/getUid.ts";
+import { useNavigate } from "react-router-dom";
+import { CreateProjectModal, Project } from "@/components/CreateProjectModal";
+import { useModal } from "@/hooks/useModal.tsx";
+import { PROJECTS_KEY } from "@/utils/constants.ts";
 
 const stopPropagation: MouseEventHandler = e => e.stopPropagation();
 
 const getInitialProjects = () => {
   const projects = localStorage.getItem(PROJECTS_KEY);
-  return JSON.parse(projects ?? '[]') as Project[];
-}
+  return JSON.parse(projects ?? "[]") as Project[];
+};
 
 const gutter: [Gutter, Gutter] = [20, 20];
 
@@ -22,17 +22,27 @@ const MainPage1: FC = () => {
   const { open, onOpen, onClose } = useModal();
   const [projects, setProjects] = useState<Project[]>(getInitialProjects);
 
+  const handleSubmit = useCallback(
+    (values: Omit<Project, "id">) => {
+      setProjects(p => {
+        const newProjects = [
+          {
+            id: getUid(),
+            height: +values.height,
+            width: +values.width,
+            name: values.name
+          },
+          ...p
+        ];
+        localStorage.setItem(PROJECTS_KEY, JSON.stringify(newProjects));
+        return newProjects;
+      });
 
-  const handleSubmit = useCallback((values: Omit<Project, 'id'>) => {
-    setProjects(p => {
-      const newProjects = [{ id: getUid(), height: +values.height, width: +values.width, name: values.name }, ...p];
-      localStorage.setItem(PROJECTS_KEY, JSON.stringify(newProjects));
-      return newProjects;
-    });
-
-    form.resetFields();
-    onClose();
-  }, [form, onClose]);
+      form.resetFields();
+      onClose();
+    },
+    [form, onClose]
+  );
 
   const handleDelete = (id: string) => {
     setProjects(prevState => {
@@ -40,45 +50,62 @@ const MainPage1: FC = () => {
       localStorage.setItem(PROJECTS_KEY, JSON.stringify(newProjects));
       return newProjects;
     });
-  }
+  };
 
   return (
-    <main className="max-w-[1500px] mx-auto mt-32">
-      <div className={`flex ${projects.length ? 'justify-between' : 'items-center flex-col'} mb-8`}>
+    <main className='max-w-[1500px] mx-auto mt-32'>
+      <div
+        className={`flex ${
+          projects.length ? "justify-between" : "items-center flex-col"
+        } mb-8`}
+      >
         <Typography.Title>
-          {!projects.length ? 'Проекты отсутствуют' : 'Мои проекты'}
+          {!projects.length ? "Проекты отсутствуют" : "Мои проекты"}
         </Typography.Title>
-        <Button onClick={onOpen} className="!bg-green-500" type="primary" size="large">
+        <Button
+          onClick={onOpen}
+          className='!bg-green-500'
+          type='primary'
+          size='large'
+        >
           Новый проект
         </Button>
       </div>
 
-
       <Row gutter={gutter} wrap>
         {projects.map(project => (
           <Col span={6} key={project.id}>
-            <Card onClick={() => navigate(`/projects/${project.id}`)} className="cursor-pointer">
-              <Typography.Title level={2} className="!mb-12">
+            <Card
+              onClick={() =>
+                navigate(`/projects/${project.id}`, { state: project })
+              }
+              className='cursor-pointer'
+            >
+              <Typography.Title level={2} className='!mb-12'>
                 {project.name}
               </Typography.Title>
 
-              <div className="flex justify-between">
-                <Link to={`/projects/${project.id}`}>
-                  <Button color="primary" type="primary" size="large">
-                    Открыть
-                  </Button>
-                </Link>
-
+              <div className='flex justify-between'>
+                <Button
+                  color='primary'
+                  type='primary'
+                  size='large'
+                  onClick={() =>
+                    navigate(`/projects/${project.id}`, { state: project })
+                  }
+                >
+                  Открыть
+                </Button>
 
                 <div onClick={stopPropagation}>
                   <Popconfirm
-                    title="Удалить проект"
-                    description="Вы уверены, что хотите удалить проект?"
-                    okText="Удалить"
-                    cancelText="Отмена"
+                    title='Удалить проект'
+                    description='Вы уверены, что хотите удалить проект?'
+                    okText='Удалить'
+                    cancelText='Отмена'
                     onConfirm={() => handleDelete(project.id)}
                   >
-                    <Button danger size="large">
+                    <Button danger size='large'>
                       Удалить
                     </Button>
                   </Popconfirm>
@@ -89,9 +116,14 @@ const MainPage1: FC = () => {
         ))}
       </Row>
 
-      <CreateProjectModal open={open} onClose={onClose} form={form} handleSubmit={handleSubmit} />
+      <CreateProjectModal
+        open={open}
+        onClose={onClose}
+        form={form}
+        handleSubmit={handleSubmit}
+      />
     </main>
   );
-}
+};
 
 export const MainPage = memo(MainPage1);
