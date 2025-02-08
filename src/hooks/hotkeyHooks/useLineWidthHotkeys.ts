e9d0716c489e2009873b75f47892d1a95401819c
Calorific <kknd3390@mail.ru>
2025-02-08 23:18:41 +0700

    fix: alt вместо ctrl

--- src/hooks/hotkeyHooks/useLineWidthHotkeys.ts
+++ src/hooks/hotkeyHooks/useLineWidthHotkeys.ts
@@ -9,7 +9,7 @@ export const useLineWidthHotkeys = () => {
   const d = useAppDispatch();
 
   const handleKeyDown = useCallback((e: KeyboardEvent) => {
-    if (e.ctrlKey) {
+    if (e.altKey) {
       if (e.key === '+') {
         d(lineWidthUp());
         e.preventDefault();
@@ -21,8 +21,25 @@ export const useLineWidthHotkeys = () => {
     }
   }, [d]);
 
+  const handleWheel = useCallback((e: WheelEvent) => {
+    if (e.altKey) {
+      e.preventDefault();
+      e.stopPropagation();
+      if (e.deltaY > 0) {
+        d(lineWidthDown());
+      } else {
+        d(lineWidthUp());
+      }
+    }
+  }, [d]);
+
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDown);
     return () => window.removeEventListener('keydown', handleKeyDown);
   }, [handleKeyDown]);
+
+  useEffect(() => {
+    window.addEventListener('wheel', handleWheel, { passive: false });
+    return () => window.removeEventListener('wheel', handleWheel);
+  }, []);
 };
