7bf1f494bd42ecd4e9be9ca0941f08dd1cdf3d04
eg-bond <eg-bond@yandex.ru>
2025-02-16 11:39:23 +0300

    last

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,15 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': '/src',
-      },
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
     },
-    server: {
-      open: true,
-    },
-  };
-
-  if (command === 'serve') {
-    return {
-      ...config,
-      base: '/',
-    };
-  } else {
-    return {
-      ...config,
-      base: '/graphic_editor_project',
-    };
-  }
+  },
+  server: {
+    open: true,
+  },
+  base: '/graphic_editor_project',
 });
