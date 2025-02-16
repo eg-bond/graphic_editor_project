2bcf05d9d6827e71e9a5065c5d19fb2fb600003e
eg-bond <eg-bond@yandex.ru>
2025-02-16 11:12:27 +0300

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,28 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
     },
-  },
-  server: {
-    open: true,
-  },
-  base: '/graphic_editor_project',
+    server: {
+      open: true,
+    },
+  };
+
+  if (command === 'serve') {
+    return {
+      ...config,
+      base: '/',
+    };
+  } else {
+    return {
+      ...config,
+      base: '/graphic_editor_project',
+    };
+  }
 });
